b7ee57de9271ff4fe203aadc04dc8aca78212b7c
Dima <workday2006@yandex.ru>
2018-05-19 08:57:54 +0300

    init

--- src/main/java/com/IdentityApplication.java
+++ src/main/java/com/IdentityApplication.java
@@ -2,13 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by dima on 21.01.18.
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class IdentityApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdentityApplication.class, args) ;
